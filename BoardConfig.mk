b24bc21b8e47e0b22812db49468a4a8fe6d94ebc
Michael Bestas <mkbestas@gmail.com>
2023-01-31 20:33:42 +0200

    Set platform security patch level
    Change-Id: Ied8a5b9d0a36b189121d9f3abc8e5c3b7516b60e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOOT_SECURITY_PATCH := 2026-08-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
+# Calyx
+PLATFORM_SECURITY_PATCH := 2024-10-05
+
 # SEPolicy
 include device/calyx/sepolicy/libion/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
